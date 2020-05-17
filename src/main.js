99fce4307b7665b51b4ba9e59b22d89d74e5c224
Andrew <andkross@yandex.ru>
2020-05-17 22:35:36 +0300

    добавляет компонент загрузки страницы

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
 import API from "./api.js";
 import ProfileRatingComponent from "./components/profile-rating.js";
+import PageLoadingComponent from "./components/page-loading.js";
 import FilmsSectionComponent from "./components/films-section.js";
 import FooterStatsComponent from "./components/footer-stats.js";
 import StatsComponent from "./components/stats.js";
 import PageController from "./controllers/page-controller.js";
 import FilterController from "./controllers/filter-controller.js";
 import FilmsModel from "./models/films-model.js";
-import {render, RenderPosition} from "./utils/render.js";
+import {render, RenderPosition, remove} from "./utils/render.js";
 
-const AUTHORIZATION = `Basic HfJ6w1Ufa215Ajs`;
+const AUTHORIZATION = `Basic HfJ6w1Ufa215Ajs3Y`;
 
 const siteHeaderElement = document.querySelector(`.header`);
 const siteMainElement = document.querySelector(`.main`);
@@ -24,6 +25,8 @@ filterController.render();
 
 const filmsSectionComponent = new FilmsSectionComponent();
 render(siteMainElement, filmsSectionComponent);
+const pageLoadingComponent = new PageLoadingComponent();
+render(filmsSectionComponent.getFilmsListElement(), pageLoadingComponent);
 
 const pageController = new PageController(filmsSectionComponent, filmsModel);
 
@@ -44,6 +47,7 @@ filterController.getFilterComponent().setFilterChangeHandler(() => {
 
 api.getFilms()
   .then((films) => {
+    remove(pageLoadingComponent);
     filmsModel.setFilms(films);
     pageController.render();
   });
